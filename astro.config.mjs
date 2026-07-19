e4291df7c5fc62ac7d73997ae01108a5e5e690da
Oliver Spryn <2717340+oliverspryn@users.noreply.github.com>
2026-07-19 15:21:23 -0400

    Ignore AI tools, minify output

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,5 +5,10 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  vite: {
+    build: {
+      minify: true
+    }
+  }
 });
